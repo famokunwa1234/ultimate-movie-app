a7bcbdaf5e8cbc441fbe154609411300833c8fda
famokunwa1234 <vfamokunwa@gmail.com>
2020-02-28 03:37:19 -0800

    Updates

---
+++ precache-manifest.af0ad646b9d4af9c44fce8d5f30119a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d13d01a85fd27d53266b55afbc44221a",
+    "url": "/ultimate-movie-app/index.html"
+  },
+  {
+    "revision": "01be9e2e5dd26427b3cf",
+    "url": "/ultimate-movie-app/static/css/main.e2a4565b.chunk.css"
+  },
+  {
+    "revision": "d59c2799db10b93e5990",
+    "url": "/ultimate-movie-app/static/js/2.cd524cc9.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/ultimate-movie-app/static/js/2.cd524cc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01be9e2e5dd26427b3cf",
+    "url": "/ultimate-movie-app/static/js/main.24291d7e.chunk.js"
+  },
+  {
+    "revision": "62f43f1d68f72b2b508e",
+    "url": "/ultimate-movie-app/static/js/runtime-main.f5352984.js"
+  }
+]);
